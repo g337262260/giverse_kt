f13a9dc0c13bd986d795d8c4933f4a47fab59b49
GuoW <g337262260@163.com>
2021-01-22 11:30:32 +0800

    新建实体类

--- Giverse_kt/src/main/kotlin/com/guow/Giverse_kt/entity/Diary.kt
+++ Giverse_kt/src/main/kotlin/com/guow/Giverse_kt/entity/Diary.kt
@@ -3,20 +3,23 @@ package com.guow.Giverse_kt.entity
 import java.util.*
 import javax.persistence.*
 
+
 /**
  * @Author BroG
- * @Description : 文章实体类
- * @Date 上午10:51 2021/1/22
+ * @Description : 日记实体类
+ * @Date 上午10:42 2021/1/22
  **/
 @Entity
-class Article {
+class Diary {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long = -1
     @Version
     var version: Long = 0
     var title: String = ""
-    var type:Int = 0    //文章分类
+    //分类：待定
+    var type:Int = 1
     var content: String = ""
     var author: String = ""
     var cover:String = ""
@@ -25,11 +28,4 @@ class Article {
     var gmtModified: Date = Date()
     var isDeleted: Int = 0 //1 Yes 0 No
     var deletedDate: Date = Date()
-
-
-    override fun toString(): String {
-        return "Article(id=$id, version=$version, title='$title', type=$type, content='$content', author='$author', cover='$cover', follow_up='$follow_up', gmtCreated=$gmtCreated, gmtModified=$gmtModified, isDeleted=$isDeleted, deletedDate=$deletedDate)"
-    }
-
-
 }
