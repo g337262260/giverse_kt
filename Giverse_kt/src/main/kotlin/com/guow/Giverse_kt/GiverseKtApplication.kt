3874c5febb20dda3c14e4f3c46450cb88441e92b
GuoW <g337262260@163.com>
2021-01-22 09:13:47 +0800

    创建项目

---
+++ Giverse_kt/src/main/kotlin/com/guow/Giverse_kt/GiverseKtApplication.kt
@@ -0,0 +1,26 @@
+package com.guow.Giverse_kt
+
+import com.guow.Giverse_kt.dao.ArticleRepository
+import com.guow.Giverse_kt.entity.Article
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import java.util.*
+
+@SpringBootApplication
+class GiverseKtApplication{
+
+//	@Bean
+//	fun init(repository: ArticleRepository) = CommandLineRunner {
+//		val article: Article = Article()
+//		article.author = "Kotlin"
+//		article.title = "极简Kotlin教程 ${Date()}"
+//		article.content = "Easy Kotlin ${Date()}"
+//		repository.save(article)
+//	}
+}
+
+fun main(args: Array<String>) {
+	runApplication<GiverseKtApplication>(*args)
+}
